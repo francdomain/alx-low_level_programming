aacabef203911e86f05b1bae6ffa82f22a983ac4
francdomain <cfrancmail@gmail.com>
2022-06-30 11:06:03 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h> 
-/**                                                                                                                              
+#include <stdio.h>
+/**
  * main - Entry point
- * Description: Using print funtion                                                                                                              * Return: Always 0 (Success)                                                                                                                   
- */                                                                                                                                             
-int main(void)                                                                                                                                  
+ * Description: Using printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}  
+}
