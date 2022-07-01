73cc1cc35d92280ed705f08506b239737a166857
francdomain <cfrancmail@gmail.com>
2022-07-01 12:32:08 -0700

    lower and then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+
+char c = 'A';
+while (c <= 'Z')
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+
+return (0);
+}
