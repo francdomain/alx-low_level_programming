63ac19d64a8e6f9145fe08011d8b41cf22de9189
francdomain <cfrancmail@gmail.com>
2022-07-01 16:32:48 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,24 @@
 int main(void)
 {
 int i, j, k;
-for (i = 0; i < 100; i++)
+
+i = 0;
+while (i < 100)
 {
-	j = i / 10;
-	k = i % 10;
+	j = i % 10;
+	k = i / 10;
+	if (k < )
+	{
 	putchar(j + '0');
 	putchar(k + '0');
 	
-	if (i < 99)
+	if (i < 89)
 	{
 		putchar(44);
 		putchar(32);
 	}
+	}
+	i++;
 }
 putchar('\n');
 return (0);
