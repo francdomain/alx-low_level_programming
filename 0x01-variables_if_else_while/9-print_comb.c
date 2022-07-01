7beab5104f625b4bfb75170a489d994caa7f5982
francdomain <cfrancmail@gmail.com>
2022-07-01 15:15:06 -0700

    combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+	putchar(i + '0');
+	if (i < 9)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
