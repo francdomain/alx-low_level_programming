591d985afbb401cfb65e44008400aa91652d0f77
francdomain <cfrancmail@gmail.com>
2022-07-01 12:52:32 -0700

    alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase except q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+	if (c != 'q' && c != 'e')
+		putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
