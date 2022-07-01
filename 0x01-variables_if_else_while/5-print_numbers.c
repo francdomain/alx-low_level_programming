8ac3316593d48ca1173658d254394b08f9ce2553
francdomain <cfrancmail@gmail.com>
2022-07-01 13:03:55 -0700

    single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+putchar('\n');
+return (0);
+}
