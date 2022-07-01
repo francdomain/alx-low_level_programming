7cb0ca57ef5c951e7da36d71beff4f497afa0507
francdomain <cfrancmail@gmail.com>
2022-07-01 15:39:28 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+int 1, j, k;
+for (i = 0; i < 100; i++)
+{
+	j = i / 10;
+	k = i % 10;
+	putchar(j + '0');
+	putchar(k + '0');
+	
+	if (i < 99);
+	{
+		putchar(44);
+		putchar(32);
+	}
+}
+putchar('\n');
+return (0);
+}
