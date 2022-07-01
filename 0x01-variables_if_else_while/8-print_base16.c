1ad08fa83d9836e047b0ee48feba2aaefd114ca8
francdomain <cfrancmail@gmail.com>
2022-07-01 14:28:15 -0700

    the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+}
+for (c = 'a'; c < 'g'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
