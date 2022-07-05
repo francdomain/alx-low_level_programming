d8e7fa7ec04bfb759b0e0f960bff28a3b7018f9e
francdomain <cfrancmail@gmail.com>
2022-07-05 06:09:15 -0700

    check lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _isLower - check if c is lowercase
- * Description: checks for lowercase character
+ * _isLower - Entry point
+ * Description: checks if c is lowercase character
  * Return: 1 and 0 based on condiction
  */
+
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
