8e9e4e67a130581842b22f888336d0afac55ca3a
francdomain <cfrancmail@gmail.com>
2022-07-05 04:40:55 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_puthchar('\n');
+}
