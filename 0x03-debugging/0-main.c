2cd54157f9ca83748bbca493ae1206df11f03cff
francdomain <cfrancmail@gmail.com>
2022-07-06 19:06:00 +0000

    debugging task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-void positive_or_negative(int i)
-{
-int n;
-srand(time(0));
-n = i;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
-}
-return (0);
-}
+#include "main.h"
 
 /**
-* main - Entry point
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -27,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
