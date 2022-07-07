c02e98f6f450b8db56105c81ee7981bb483d767c
francdomain <cfrancmail@gmail.com>
2022-07-07 07:54:48 -0700

    check isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,9 @@
 /**
  * _isupper - function that checks for uppercase characters
  * @c: int type number
- * Return: 1 if uppercase, otherwise 0
+ * Return: success
  */
 int _isupper(int c)
 {
-	if (c > 'A' && c < 'Z')
-		return (1);
-
-	return (0);
-
 	return (c >= 'A' && c <= 'Z');
 }
