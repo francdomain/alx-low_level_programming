3efff56b97462041606b01e9f04ae0f887e302dd
francdomain <cfrancmail@gmail.com>
2022-07-07 06:49:47 +0000

    remaining_day of the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -11,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 != 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
