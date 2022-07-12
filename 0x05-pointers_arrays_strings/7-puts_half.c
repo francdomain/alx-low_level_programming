a6bd1d803f7e35ed039c8053cd1a614c21f84632
francdomain <cfrancmail@gmail.com>
2022-07-12 06:12:48 -0700

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: the string involved
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	n = (strlen(str) - 1) / 2;
+	for (i = 0; i <= n; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
