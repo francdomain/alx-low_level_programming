4e26cd6f1c33c9b6fd6049ba5eea7a9d1d1ea261
francdomain <cfrancmail@gmail.com>
2022-07-12 03:24:47 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s:string poiter to be passed
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
