869193522877931bc62f232bd23e029e77b9d7b4
francdomain <cfrancmail@gmail.com>
2022-07-14 05:02:45 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function that concatenates two strings and
+ * use n bytes form src.
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: pointer to result
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	result = _strncat(dest, src, n);
+
+	return (result);
+
+}
