cc00936e92fc209baecd94b59c4a3f3e8c63fd30
francdomain <cfrancmail@gmail.com>
2022-07-14 07:20:02 -0700

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
