7cddb39c64b7fca1bcb62940c04557c3f9b9bf2f
francdomain <cfrancmail@gmail.com>
2022-07-18 08:23:24 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function that sets the value of a pointer to a char.
+ * @s: string
+ * @to: character
+ * Return: Success
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
