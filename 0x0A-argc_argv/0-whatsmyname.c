461ebd434809ed114d1e443b2035daeec961bdb9
francdomain <cfrancmail@gmail.com>
2022-07-22 06:19:44 -0700

    function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - function that prints its name
+  * @argc: argument count
+  * @argv: argument array of commands listed
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
