ee90347b11f77b21711f0a2a5fffaf03be56a21d
francdomain <cfrancmail@gmail.com>
2022-07-24 11:20:21 -0700

    create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars
+ * @size: length of array s
+ * @c: sting character
+ * Return: A pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
