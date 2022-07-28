7309c0f5b409a5748afc393cc7386bb6c277e52c
francdomain <cfrancmail@gmail.com>
2022-07-28 06:08:15 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,50 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string paeameter 1
- * @s2: string parameter 2
- * @n: bytes to include of s2
- * Return: NULL if fail, else pointer to malloc memory
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int strlen1, i, c;
+	char *concat;
+	unsigned int len = n, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	strlen1 = (unsigned int)_strlen(s1);
-	p = malloc((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
-	{
-		if (i < strlen1)
-			p[i] = s1[i];
-		else
-			p[i] = s2[c++];
-	}
-	p[i] = '\0';
 
-	return (p);
-}
+	len = 0;
 
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
+	for (i = 0; s1[i]; i++)
+		concat[len++] = s1[i];
 
-int _strlen(char *s)
-{
-	int i;
+	for (i = 0; s2[i] && i < n; i++)
+		concat[len++] = s2[i];
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
+	concat[len] = '\0';
+
+	return (concat);
 }
