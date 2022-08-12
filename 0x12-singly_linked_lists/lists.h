1f8ccece75740bc253621d3334703c57f3fb38e0
francdomain <cfrancmail@gmail.com>
2022-08-11 17:00:06 -0700

    elements of a list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,7 +18,6 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
